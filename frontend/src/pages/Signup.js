8176e282bbc25280dd092e87d65af330db692495
Daniel <callejasmark63@gmail.com>
2024-11-22 12:38:57 +0800

    added login

---
+++ frontend/src/pages/Signup.js
@@ -0,0 +1,51 @@
+import { useState } from "react"
+import { useSignup } from "../hooks/useSignup"
+
+const Signup = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const {signup, error, isloading} = useSignup('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        await signup(email, password, firstName, lastName)
+    }
+
+    return(
+        <form className="signup" onSubmit={handleSubmit}>
+            <h3>Sign Up</h3>
+            <label>First Name:</label>
+            <input 
+            type="text"
+            onChange={(e) => setFirstName(e.target.value)}
+            value={firstName}
+            />
+            <label>Last Name:</label>
+            <input 
+            type="text"
+            onChange={(e) => setLastName(e.target.value)}
+            value={lastName}
+            />
+            <label>Email:</label>
+            <input 
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            />
+            <label>Password:</label>
+            <input 
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            />
+
+            <button disabled={isloading}>Sign Up</button>
+            {error && <div className="error">{error}</div>}
+        </form>
+    )
+}
+
+export default Signup
