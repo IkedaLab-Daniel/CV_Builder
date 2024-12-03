bddc017474ea4b64c732598e7ab0b855dde691b5
Daniel <callejasmark63@gmail.com>
2024-12-03 09:51:58 +0800

    Remove demo components/contexts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,19 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { WorkoutsContextProvider } from './context/WorkoutContext';
 import { ResumeContextProvider } from './context/ResumeContext';
 import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <WorkoutsContextProvider>
       <AuthContextProvider>
         <ResumeContextProvider>
           <App />
         </ResumeContextProvider>
       </AuthContextProvider>
-    </WorkoutsContextProvider>
   </React.StrictMode>
 )
